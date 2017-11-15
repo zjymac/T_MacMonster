cdc2a0522658fd9c3384344bbcda5550fc5cdb0a
zjy <853731069@qq.com>
2017-11-15 15:23:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a58/t_macmonster/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a58.t_macmonster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
